d6380a2d4694e16d81d00faac3da9d0110e4d430
AndreasKarasenko <andreas.karasenko@googlemail.com>
2024-02-16 18:24:18 +0100

    added examples surrounding imbalance

--- examples/metrics/balancing.py
+++ examples/metrics/balancing.py
@@ -35,3 +35,29 @@ y_true = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2])
 y_pred = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 f1 = metrics.f1_score(y_true, y_pred, average="weighted")
 print(f"Weighted multi-class F1 score (heavily biased): {f1:.2f}")
+
+# we can also demonstrate the effect of class imbalance on the weighted f1 score by using the eval_metrics function
+# we can see that the weighted f1 score is biased towards the majority class
+# this is also reflected in the precision and recall scores
+def eval_metrics(
+    y_true: np.ndarray, y_pred: np.ndarray, weighting: str = "binary"
+):
+    """Compute evaluation metrics for multi-class classification.
+
+    Args:
+        y_true: true labels
+        y_pred: predicted labels
+        weighting: type of averaging for multi-class classification
+    """
+    f1 = []
+    precision = []
+    recall = []
+    for i in ("macro", "micro", "weighted"):
+        f1.append(metrics.f1_score(y_true, y_pred, average=i))
+        precision.append(metrics.precision_score(y_true, y_pred, average=i))
+        recall.append(metrics.recall_score(y_true, y_pred, average=i))
+    acc = metrics.accuracy_score(y_true, y_pred)
+    return acc, f1, precision, recall
+
+for i in eval_metrics(y_true, y_pred):
+    print(i)
