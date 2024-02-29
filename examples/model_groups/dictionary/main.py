fc8f1b8ecf07943c4a77452450d3a2484211c111
AndreasKarasenko <andreas.karasenko@googlemail.com>
2024-02-29 09:16:54 +0100

    added vader example

--- examples/model_groups/dictionary/main.py
+++ examples/model_groups/dictionary/main.py
@@ -1,13 +1,23 @@
+# load libraries
 import pandas as pd
 from utils.dataloader import googleplay
-from models import DICTIONARY_MODELS
+from utils.describe import describe
 
 # load the data
 df = googleplay(config=None, path="./data/ikea_en.csv")
 df = df.iloc[:1000,:] # subsample for faster processing
+print(describe(df, "content", score_col="score"))
 
 # load the model
+from models import DICTIONARY_MODELS
+
 model = DICTIONARY_MODELS["Vader"] # gets the function for the model
 model_instance = model()
 
 # calculate the sentiment
+df["sentiment"] = df["content"].apply(lambda x: model_instance.polarity_scores(x)["compound"])
+
+# plot sentiment
+import matplotlib.pyplot as plt
+df["sentiment"].hist()
+plt.show()
