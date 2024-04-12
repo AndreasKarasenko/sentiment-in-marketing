78f75892db7603ba5900f11b4eae3d8fed6abe0e
AndreasKarasenko <andreas.karasenko@googlemail.com>
2024-04-12 15:47:41 +0200

    added experimental eval function

--- eval_ml.py
+++ eval_ml.py
@@ -1,22 +1,28 @@
 # main script to eval all normal machine learning based models
 # NB, LR, SVM, DT, RF, XGBoost
 
-# import utilities
-import warnings
 import argparse
+import json
 import os
 import time
-import json
-import pandas as pd
-from datetime import datetime
-from utils.optimize import run_gridsearchcv
-from utils.save_results import save_results
 
-### import evaluation functions
-from utils.eval import eval_metrics
+# import utilities
+import warnings
+from datetime import datetime
+
+# typing
+from typing import Any, Callable, Dict, List
+
+import pandas as pd
 
 ### import the configurations
 from config.utils_config.argparse_args import arguments
+
+### import evaluation functions
+from utils.eval import eval_metrics
+from utils.optimize import run_gridsearchcv
+from utils.preprocess import TextPreprocessor
+from utils.save_results import save_results
 
 warnings.filterwarnings("ignore")
 os.environ["PYTHONWARNINGS"] = "ignore"
@@ -50,73 +56,94 @@ target_vars = target_vars["target_vars"]
 ### import the models dict
 from models import MODELS
 
-### import preprocessing functions
-from sklearn.base import TransformerMixin
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-import string
+# for i in datasets:
+#     train = pd.read_csv(args.data_dir + i + "_train.csv")
+#     train.dropna(inplace=True)
+#     test = pd.read_csv(args.data_dir + i + "_test.csv")
+#     test.dropna(inplace=True)
+
+#     X_train = train[input_vars]
+#     X_test = test[input_vars]
+#     # apply the text preprocessor to the text data
+#     # If we don't do this we incur a 4s penalty for each fold
+#     X_train = TextPreprocessor().transform(X_train)
+#     X_test = TextPreprocessor().transform(X_test)
+
+#     y_train = train[target_vars]
+#     y_test = test[target_vars]
+
+#     # get the model
+#     for model_name, model_func in MODELS.items():
+#         model_instance = model_func()
+#         start = time.time()
+#         grid, metrics = run_gridsearchcv(
+#             model_instance,
+#             search_space[model_name]["hyperparameters"],
+#             X_train,
+#             y_train,
+#             X_test,
+#             y_test,
+#             verbose=args.verbose,
+#             n_jobs=args.njobs,
+#         )
+#         end = time.time()
+#         walltime = end - start
+#         print(grid.best_params_)
+#         print(metrics[-1])
+#         filename = (
+#             model_name + "_" + i + "_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+#         )
+#         save_results(filename, model_name, i, metrics, args, walltime, grid)
 
 
-# create a text preprocessor class to preprocess the text data once before the hp tuning
-# since the preprocessor does not learn there is no risk of overfitting
-# all other transformation is still done in folds
-class TextPreprocessor(TransformerMixin):
-    def __init__(self):
-        self.lemmatizer = WordNetLemmatizer()
+def run_eval(
+    datasets: List[str],
+    model_dict: Dict[str, Callable[[], Any]],
+    args: argparse.Namespace,
+):
+    for i in datasets:
+        train = pd.read_csv(args.data_dir + i + "_train.csv")
+        train.dropna(inplace=True)
+        test = pd.read_csv(args.data_dir + i + "_test.csv")
+        test.dropna(inplace=True)
 
-    def fit(self, X, y=None):
-        return self
+        X_train = train[input_vars]
+        X_test = test[input_vars]
+        X_train = TextPreprocessor().fit_transform(X_train)
+        X_test = TextPreprocessor().transform(X_test)
 
-    def transform(self, X, y=None):
-        X_transformed = []
-        for document in X:
-            # Lowercase
-            document = document.lower()
-            # Remove punctuation
-            document = document.translate(str.maketrans("", "", string.punctuation))
-            # Tokenize
-            words = word_tokenize(document)
-            # Lemmatize
-            words = [self.lemmatizer.lemmatize(word) for word in words]
-            X_transformed.append(" ".join(words))
-        return X_transformed
+        y_train = train[target_vars]
+        y_test = test[target_vars]
+
+        # get the model
+        for model_name, model_func in model_dict.items():
+            model_instance = model_func()
+            start = time.time()
+            grid, metrics = run_gridsearchcv(
+                model_instance,
+                search_space[model_name]["hyperparameters"],
+                X_train,
+                y_train,
+                X_test,
+                y_test,
+                verbose=args.verbose,
+                n_jobs=args.njobs,
+            )
+            end = time.time()
+            walltime = end - start
+            print(grid.best_params_)
+            print(metrics[-1])
+            filename = (
+                model_name
+                + "_"
+                + i
+                + "_"
+                + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            )
+            save_results(filename, model_name, i, metrics, args, walltime, grid)
+
+    return 1
 
 
-for i in datasets:
-    train = pd.read_csv(args.data_dir + i + "_train.csv")
-    train.dropna(inplace=True)
-    test = pd.read_csv(args.data_dir + i + "_test.csv")
-    test.dropna(inplace=True)
-
-    X_train = train[input_vars]
-    X_test = test[input_vars]
-    # apply the text preprocessor to the text data
-    # If we don't do this we incur a 4s penalty for each fold
-    X_train = TextPreprocessor().transform(X_train)
-    X_test = TextPreprocessor().transform(X_test)
-
-    y_train = train[target_vars]
-    y_test = test[target_vars]
-
-    # get the model
-    for model_name, model_func in MODELS.items():
-        model_instance = model_func()
-        start = time.time()
-        grid, metrics = run_gridsearchcv(
-            model_instance,
-            search_space[model_name]["hyperparameters"],
-            X_train,
-            y_train,
-            X_test,
-            y_test,
-            verbose=args.verbose,
-            n_jobs=args.njobs,
-        )
-        end = time.time()
-        walltime = end - start
-        print(grid.best_params_)
-        print(metrics[-1])
-        filename = (
-            model_name + "_" + i + "_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        )
-        save_results(filename, model_name, i, metrics, args, walltime, grid)
+if __name__ == "__main__":
+    run_eval(datasets, MODELS, args)
