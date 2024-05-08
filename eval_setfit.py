0c7ba3d7a1a8bb31f180d03cb9a53e4b30e5f500
AndreasKarasenko <andreas.karasenko@googlemail.com>
2024-05-08 18:04:39 +0200

    made some adjustments

--- eval_setfit.py
+++ eval_setfit.py
@@ -20,45 +20,22 @@ import pandas as pd
 ### import the configurations
 from config.utils_config.argparse_args import arguments
 
+### import the models dict
+from models import MODELS
+
 ### import evaluation functions
-from utils.dataloader import get_config_names
+from utils.dataloader import get_config_names, load_samples
 from utils.eval import eval_metrics
-from utils.optimize import run_bayesian_optimization, run_gridsearchcv
+from utils.optimize import run_gridsearchcv
 from utils.preprocess import TextPreprocessor
 from utils.save_results import save_results
 
-warnings.filterwarnings("ignore")
-os.environ["PYTHONWARNINGS"] = "ignore"
+# warnings.filterwarnings("ignore")
+# os.environ["PYTHONWARNINGS"] = "ignore"
 
 ### and parse the cli arguments
-parser = argparse.ArgumentParser(
-    description="Finds the best hyperparameters for the models"
-)
-for arg in arguments:
-    parser.add_argument(
-        arg["arg"], type=arg["type"], default=arg["default"], help=arg["help"]
-    )
 
-args = parser.parse_args()
-
-### load the search space json
-search_space = json.load(open(args.config_dir, "r"))
-
-### get the dataset names
-datasets = json.load(open(args.data_config + "dataset_config.json", "r"))
-datasets = datasets["datasets"]
-print(datasets)
-
-### get the input and target vars
-input_vars = json.load(open(args.data_config + "input_config.json", "r"))
-input_vars = input_vars["input_var"]
-
-target_vars = json.load(open(args.data_config + "target_config.json", "r"))
-target_vars = target_vars["target_vars"]
-
-### import the models dict
-from models import MODELS
-from utils.dataloader import get_config_names, load_samples
+# TODO use this paper to motivate https://arxiv.org/pdf/2308.14634
 
 
 def run_eval(
@@ -66,10 +43,12 @@ def run_eval(
     args: argparse.Namespace,
     tuning: str = "grid",
 ):
+    print("running eval")
     datasets, input_vars, target_vars = get_config_names(args)  # get the config names
     for index, name in enumerate(datasets):
+
         X_train, X_test, y_train, y_test = load_samples(
-            name, input_vars[index], target_vars[index], args
+            name, input_vars, target_vars[0], args
         )
         # get the model
         for model_name, model_func in model_dict.items():
@@ -112,9 +91,33 @@ def run_eval(
                 + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
             )
             save_results(filename, model_name, name, metrics, args, walltime, grid)
-
     return 1
 
 
 if __name__ == "__main__":
+    print("test")
+    parser = argparse.ArgumentParser(
+        description="Finds the best hyperparameters for the models"
+    )
+    for arg in arguments:
+        parser.add_argument(
+            arg["arg"], type=arg["type"], default=arg["default"], help=arg["help"]
+        )
+
+    args = parser.parse_args()
+
+    ### load the search space json
+    search_space = json.load(open(args.config_dir, "r"))
+
+    ### get the dataset names
+    datasets = json.load(open(args.data_config + "dataset_config.json", "r"))
+    datasets = datasets["datasets"]
+    print(datasets)
+
+    ### get the input and target vars
+    input_vars = json.load(open(args.data_config + "input_config.json", "r"))
+    input_vars = input_vars["input_var"]
+
+    target_vars = json.load(open(args.data_config + "target_config.json", "r"))
+    target_vars = target_vars["target_vars"]
     run_eval(MODELS, args, tuning="grid")
