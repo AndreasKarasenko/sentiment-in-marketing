1f522dc1b85ccca10ec16f5f031c7b6cd969501f
AndreasKarasenko <andreas.karasenko@googlemail.com>
2024-06-13 16:02:20 +0200

    made some adjustments to eval_skllm

--- eval_skllm.py
+++ eval_skllm.py
@@ -43,53 +43,85 @@ from skllm.config import SKLLMConfig
 from config.utils_config.openai_key import key
 
 # Configure the credentials
-SKLLMConfig.set_openai_key(key)
-
 warnings.filterwarnings("ignore")
 os.environ["PYTHONWARNINGS"] = "ignore"
-parser = argparse.ArgumentParser(
-    description="Finds the optimal Hyperparameters for the models."
-)
-for arg in arguments:
-    parser.add_argument(
-        arg["arg"], type=arg["type"], default=arg["default"], help=arg["help"]
+
+if __name__ == "__main__":
+    
+    SKLLMConfig.set_openai_key(key)
+
+    parser = argparse.ArgumentParser(
+        description="Finds the optimal Hyperparameters for the models."
     )
+    for arg in arguments:
+        parser.add_argument(
+            arg["arg"], type=arg["type"], default=arg["default"], help=arg["help"]
+        )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-model_name = "openai"
+    
+    datasets = json.load(open(args.data_config + "dataset_config.json", "r"))
+    datasets = datasets["datasets"]
+    
+    model_name = "openai"
+    
+    
+    ### get the input and target vars
+    input_vars = json.load(open(args.data_config + "input_config.json", "r"))
+    input_vars = input_vars["input_var"]
 
+    target_vars = json.load(open(args.data_config + "target_config.json", "r"))
+    target_vars = target_vars["target_vars"]
 
-train_df = pd.read_csv(os.path.join(args.data_dir, "train.csv"))
-test_df = pd.read_csv(os.path.join(args.data_dir, "test.csv"))
+    print("input_vars", input_vars)
+    print("target_vars", target_vars)
+    
+    for i in datasets:
+        train = pd.read_csv(args.data_dir + i + "_train.csv") # type: ignore
+        test = pd.read_csv(args.data_dir + "/subsamples/" + i + "_test.csv") # type: ignore
+        
+        train = train.loc[:, [input_vars, target_vars[0]]]
+        test = test.loc[:, [input_vars, target_vars[0]]]
+        
+        train.rename(columns={input_vars: "text", target_vars[0]: "label"}, inplace=True)
+        test.rename(columns={input_vars: "text", target_vars[0]: "label"}, inplace=True)
+        
+        train.dropna(inplace=True)
+        test.dropna(inplace=True)
+    
+    
+    ### get the input and target vars
+    train_df = pd.read_csv(os.path.join(args.data_dir, "train.csv"))
+    test_df = pd.read_csv(os.path.join(args.data_dir, "test.csv"))
 
-input_vars = json.load(
-    open(args.data_config + "input_config.json", "r")
-)  # for specifying input
-input_vars = input_vars["input_var"]
+    input_vars = json.load(
+        open(args.data_config + "input_config.json", "r")
+    )  # for specifying input
+    input_vars = input_vars["input_var"]
 
-target_vars = json.load(
-    open(args.data_config + "target_config.json", "r")
-)  # for specifying target
-target_vars = target_vars["target_vars"]
+    target_vars = json.load(
+        open(args.data_config + "target_config.json", "r")
+    )  # for specifying target
+    target_vars = target_vars["target_vars"]
 
-for i in target_vars:
-    samples = get_samples(train_df, n_samples=4, label_col=i)
+    for i in target_vars:
+        samples = get_samples(train_df, n_samples=4, label_col=i)
 
-    clf = FewShotGPTClassifier().fit(
-        samples[input_vars].tolist(), samples[i].tolist()
-    )
-    # clf = DynamicFewShotGPTClassifier(model="gpt-3.5-turbo")
-    # clf.fit()
+        clf = FewShotGPTClassifier().fit(
+            samples[input_vars].tolist(), samples[i].tolist()
+        )
+        # clf = DynamicFewShotGPTClassifier(model="gpt-3.5-turbo")
+        # clf.fit()
 
-    X_test = test_df[input_vars].tolist()
-    y_test = test_df[i].tolist()
+        X_test = test_df[input_vars].tolist()
+        y_test = test_df[i].tolist()
 
-    preds = clf.predict(
-        X_test,
-    )
-    np.save("preds_" + i + ".npy", preds)
-    metrics = eval_metrics(y_test, preds)
+        preds = clf.predict(
+            X_test,
+        )
+        np.save("preds_" + i + ".npy", preds)
+        metrics = eval_metrics(y_test, preds)
 
-    filename = model_name + "_" + i + "_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    save_results(filename, model_name, i, metrics, args)
+        filename = model_name + "_" + i + "_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        save_results(filename, model_name, i, metrics, args)
